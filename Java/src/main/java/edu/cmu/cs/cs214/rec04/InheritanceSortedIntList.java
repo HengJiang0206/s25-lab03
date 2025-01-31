4a071ff8cea69ec3ef4e9c4b0ef79c9bd365ee2d
Heng Jiang <hengj@andrew.cmu.edu>
2025-01-31 14:13:05 -0500

    implement totalAdd using inheritance

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -6,11 +6,26 @@ package edu.cmu.cs.cs214.rec04;
  * with the current size, which goes down when an element is removed)
  * and exports an accessor (totalAdded) for this count.
  *
- * @author Nora Shoemaker
+ * @author Heng Jiang
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList {
+    int totalAdded = 0;
     // Write your implementation below with API documentation
+    @Override
+    public boolean add(int num) {
+        super.add(num);
+        totalAdded += 1;
+        return true;
+    }
 
+    @Override
+    public boolean addAll(IntegerList list) {
+        return super.addAll(list);
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
