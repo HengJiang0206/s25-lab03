3a35f32ebd3b90feb99092beaba30e18baddb7d6
Heng Jiang <hengj@andrew.cmu.edu>
2025-01-31 14:12:52 -0500

    implement totalAdded using delegation

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,46 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList {
     // Write your implementation below with API documentation
+    SortedIntList sortedList = new SortedIntList();
+    int totalAdded = 0;
 
+    @Override
+    public boolean add(int num) {
+        sortedList.add(num);
+        totalAdded += 1;
+        return true;
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        sortedList.addAll(list);
+        totalAdded += list.size();
+        return true;
+    }
+
+    @Override
+    public int get(int index) {
+        return sortedList.get(index);
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return sortedList.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return sortedList.removeAll(list);
+    }
+
+    @Override
+    public int size() {
+        return sortedList.size();
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
